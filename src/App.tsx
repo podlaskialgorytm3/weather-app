7d24a723728d0b897b6f502ec40ea8401440e04a
podlaskialgorytm3 <live.m851@gmail.com>
2024-04-05 23:02:55 +0200

    feat: set color of background in app, and set new icon app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 const App = () => (
-  <div className="bg-primary h-[100vh] w-full bg-center bg-no-repeat">
+  <div className="bg-gradient-to-r from-indigo-400 to-cyan-400 h-[100vh] w-full bg-center bg-no-repeat">
   </div>
 )
 
