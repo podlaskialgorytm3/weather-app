02491aae5385c12dabe891217a1cb15daab91813
podlaskialgorytm3 <live.m851@gmail.com>
2024-04-06 23:20:12 +0200

    feat: improving of search bar component through adding special search symbol

--- src/features/search/components/search-bar.tsx
+++ src/features/search/components/search-bar.tsx
@@ -1,11 +1,25 @@
 import OutlinedInput from '@mui/material/OutlinedInput';
+import SearchIcon from '@mui/icons-material/Search';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
 
 export const SearchBar = () => {
+    const handleSearch = () => {
+        console.log('Searching...');
+    };
+
     return (
         <div className="flex justify-center items-center pt-5">
             <OutlinedInput
                 placeholder="Search your location ..."
                 fullWidth
+                endAdornment={
+                    <InputAdornment position="end">
+                        <IconButton onClick={handleSearch}>
+                            <SearchIcon />
+                        </IconButton>
+                    </InputAdornment>
+                }
                 sx={{
                     width: '80%',
                     maxWidth: '500px',
@@ -17,5 +31,7 @@ export const SearchBar = () => {
                 }}
             />
         </div>
-    )
-}
+    );
+};
+
+
