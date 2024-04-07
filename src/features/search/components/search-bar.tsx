a3eb3595c9d0a4363d84d6ec4ad02894dc0afb93
podlaskialgorytm3 <live.m851@gmail.com>
2024-04-07 13:46:15 +0200

    feat: save locations to array with classic solution

--- src/features/search/components/search-bar.tsx
+++ src/features/search/components/search-bar.tsx
@@ -3,33 +3,45 @@ import SearchIcon from '@mui/icons-material/Search';
 import InputAdornment from '@mui/material/InputAdornment';
 import IconButton from '@mui/material/IconButton';
 
+import { useState } from 'react';
+
 export const SearchBar = () => {
-    const handleSearch = () => {
-        console.log('Searching...');
+    const [searchLocations, setSearchLocations] = useState<{searchLocations: string[]}>({searchLocations: []});
+    const [location, setLocation] = useState<string>('');
+
+    const handleChange = (e: { target: { value: string; }; }) => {
+        setLocation(e.target.value);
     };
+    const handleSubmit = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        setSearchLocations({searchLocations: [...searchLocations.searchLocations, location]});
+        setLocation('');
+    }
 
     return (
         <div className="flex justify-center items-center pt-5">
-            <OutlinedInput
-                placeholder="Search your location ..."
-                fullWidth
-                endAdornment={
-                    <InputAdornment position="end">
-                        <IconButton onClick={handleSearch}>
-                            <SearchIcon />
-                        </IconButton>
-                    </InputAdornment>
-                }
-                sx={{
-                    width: '80%',
-                    maxWidth: '500px',
-                    borderRadius: '999px',
-                    backgroundColor: 'white',
-                    '& input': {
-                        padding: '10px 20px',
-                    },
-                }}
-            />
+           <form onSubmit={handleSubmit}>
+                <OutlinedInput
+                    placeholder="Search your location ..."
+                    fullWidth
+                    onChange={handleChange}
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton>
+                                <SearchIcon />
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                    sx={{
+                        width: '500px',
+                        borderRadius: '999px',
+                        backgroundColor: 'white',
+                        '& input': {
+                            padding: '10px 20px',
+                        },
+                    }}
+                />
+           </form>
         </div>
     );
 };
