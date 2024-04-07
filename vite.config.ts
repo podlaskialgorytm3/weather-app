76a2247f81a1a9714d8faed42396a1e8dbb37b81
podlaskialgorytm3 <live.m851@gmail.com>
2024-04-07 17:18:28 +0200

    config: change config of project adding special env to get access to API

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+const cherryPickedKeys = [
+  "REACT_APP_API_WEATHER_KEY"
+];
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const processEnv: { [key: string]: string } = {};
+  cherryPickedKeys.forEach(key => processEnv[key] = env[key]);
+
+  return {
+    define: {
+      'process.env': processEnv
+    },
+    plugins: [react()],
+  }
 })
