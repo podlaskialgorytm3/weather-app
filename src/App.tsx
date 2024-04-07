166e2905f3df19b4f7e0b8c84c9712393366d16b
podlaskialgorytm3 <live.m851@gmail.com>
2024-04-07 15:40:10 +0200

    feat: create query client api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { Container } from "./features/container/components/container";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./api/query-client";
 
 const App = () => (
-  <Container/>
+  <QueryClientProvider client={queryClient}>
+      <Container/>
+  </QueryClientProvider>
 )
 
 export default App;
