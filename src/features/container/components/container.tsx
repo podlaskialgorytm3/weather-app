af683a6cf58589e5e18d71ecd31a8c1910beb22d
podlaskialgorytm3 <live.m851@gmail.com>
2024-04-08 11:38:09 +0200

    feat: show array with information about weather in special blocks

--- src/features/container/components/container.tsx
+++ src/features/container/components/container.tsx
@@ -7,6 +7,7 @@ import Swal from "sweetalert2"
 
 export const Container = () => {
     const [location, setLocation] = useState('');
+    const [forecasts, setForecasts] = useState([] as object[]);
     const { data, isError, error, refetch, isLoading } = useFetchWeather(location);
 
     const formik = useFormik({
@@ -35,15 +36,27 @@ export const Container = () => {
         }
     
     },[isError, error])
+
+    useEffect(() => {
+        setForecasts((prevValeus) => [...prevValeus,data])
+        console.log(forecasts)
+    },[data,location])
     
     return (
-        <div className="bg-gradient-to-r from-indigo-400 to-cyan-400 h-[100vh] w-full bg-center bg-no-repeat">
+        <div className="bg-gradient-to-r from-indigo-400 to-cyan-400 h-[auto] w-full bg-center bg-no-repeat min-h-[100vh]">
             <SearchBar formik={formik} />
-            {isLoading ? (
-                <Loading size={100} />
-            ) : (
-                <div>{data && JSON.stringify(data)}</div>
-            )}
+            <div className="flex flex-col items-center justify-center">
+                {isLoading && <Loading size={100} />}
+                {forecasts && (
+                    forecasts.map((forecast, index) => (
+                        forecast !== undefined && (
+                            <div key={index} className="bg-white bg-opacity-50 p-5 rounded-lg m-5">
+                                {JSON.stringify(forecast)}
+                            </div>
+                        )
+                    ))
+                )}
+            </div>
         </div>
     );
 };
