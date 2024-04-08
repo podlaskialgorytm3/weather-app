9268d0c4bfa4787a0d71c1eeda0022d69462c3bb
podlaskialgorytm3 <live.m851@gmail.com>
2024-04-08 12:45:22 +0200

    performance: improving the function so that you can check the exact time, regardless of the zone

--- src/features/container/utils/hour-converter.ts
+++ src/features/container/utils/hour-converter.ts
@@ -1,5 +1,5 @@
-export const hourConverter = (time: number) => {
-    const sunriseTimestamp = time * 1000; // Przyjmuję, że wartość to np. 1678954321000 (timestamp)
+export const hourConverter = (time: number,timezone: number) => {
+    const sunriseTimestamp = time * 1000 - 7200000 + timezone * 1000;
     const sunriseDate = new Date(sunriseTimestamp);
     const sunriseHour = sunriseDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
