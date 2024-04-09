059f554f27ec17fd788241020e4cf6562ab98ae4
podlaskialgorytm3 <live.m851@gmail.com>
2024-04-09 21:22:02 +0200

    feat: create structur of main component related with weather card

--- src/features/container/components/weather-card.tsx
+++ src/features/container/components/weather-card.tsx
@@ -1,19 +1,39 @@
 import { hourConverter } from "../utils/hour-converter"
+import { WeatherProperty } from "./weather-property"
 
 export const WeatherCard = ({index,forecast} : {index:number,forecast: any}) => {
 
     return(
         <div key={index} className="bg-white bg-opacity-50 p-5 rounded-lg m-5">
             <div className="flex flex-col justify-start">
-                <h1 className="text-2xl font-bold">{forecast.name}</h1>
-                <h1 className="text-2xl font-bold">{forecast.main.temp}°C</h1>
-                <img src={`https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`} alt="" width={50} />
-                {forecast.sys.country && (<img src={`https://flagcdn.com/128x96/${forecast.sys.country.toLowerCase()}.png`} alt="" width={50} />)}
-                <h1 className="text-lg">{forecast.main.pressure} hPa</h1>
-                <h1>{forecast.main.humidity} %</h1>
-                <h1>{Math.floor(forecast.wind.speed * 1.609344)} km/h</h1>
-                <h1>{hourConverter(forecast.sys.sunrise,forecast.timezone)}</h1>
-                <h1>{hourConverter(forecast.sys.sunset,forecast.timezone)}</h1>
+                <div className="flex justify-start items-center">
+                {forecast.sys.country && 
+                    (<img 
+                        src={`https://flagcdn.com/128x96/${forecast.sys.country.toLowerCase()}.png`} 
+                        className="mr-3"
+                        alt="" 
+                        width={30} 
+                    />)}
+                    <h1 className="text-3xl font-bold mb-1">{forecast.name}</h1>
+                </div>
+                <div className="flex w-[100%] justify-around items-center">
+                    <div>
+                        <p className="text-5xl font-bold">{Math.round(forecast.main.temp)}°C</p>
+                    </div>
+                    <div className="flex flex-wrap justify-around w-80">
+                        <WeatherProperty property="Pressure" value={`${forecast.main.pressure} hPa`} />
+                        <WeatherProperty property="Humidity" value={`${forecast.main.humidity}%`} />
+                        <WeatherProperty property="Wind" value={`${forecast.wind.speed} m/s`} />
+                        <WeatherProperty property="Sunrise" value={hourConverter(forecast.sys.sunrise,forecast.timezone)} />
+                        <WeatherProperty property="Sunset" value={hourConverter(forecast.sys.sunset,forecast.timezone)} />
+                    </div>
+                    <div>
+                        <img src={`https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`} alt="" width={50} />
+                    </div>
+                </div>
+                
+                
+                
             </div>
     </div>
     )
