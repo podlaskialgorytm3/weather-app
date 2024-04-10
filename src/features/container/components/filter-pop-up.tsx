cc12426bfea0a7bd3c85adfb2fae2abb0fccce21
podlaskialgorytm3 <live.m851@gmail.com>
2024-04-11 00:56:21 +0200

    feat: create basic popup and placement of sliders

--- src/features/container/components/filter-pop-up.tsx
+++ src/features/container/components/filter-pop-up.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
+import Slider from '@mui/material/Slider';
 import { popupStyle, FILTER_PROPERTIES } from '../constants/filter-pop-up';
 
 
@@ -18,7 +19,22 @@ export const FilterPopUp = () => {
         onClose={handleClose}
       >
         <Box sx={popupStyle}>
-         
+            <div className='w-[400px]'>
+                {FILTER_PROPERTIES.map((property, index) => (
+                    <div key={index}>
+                        <h3 className='text-xl font-bold'>{property[0]} ({property[3]})</h3>
+                        <Slider
+                            defaultValue={[Number(property[1]),Number(property[2])]}
+                            getAriaValueText={(value) => `${value}`}
+                            valueLabelDisplay="auto"
+                            step={1}
+                            min={Number(property[1])}
+                            max={Number(property[2])}
+                        />
+                    </div>
+                ))}
+            </div>
+            <Button onClick={handleOpen} variant="outlined" color="primary" sx={{marginTop: 3}}>confirm</Button>
         </Box>
       </Modal>
     </div>
--- src/features/container/constants/filter-pop-up.ts
+++ src/features/container/constants/filter-pop-up.ts
@@ -3,17 +3,20 @@ export const popupStyle = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 800,
-    height: 600,
+    width: 600,
+    height: 400,
     bgcolor: 'background.paper',
     borderRadius: 6,
     boxShadow: 24,
+    display: 'flex',
+    flexDirection: 'column' as 'column',
+    alignItems: 'center',
     p: 4,
   };
   
 export const FILTER_PROPERTIES = [
-      ['temp',0,100],
-      ['pressure',0,2000],
-      ['humidity',0,100],
-      ['wind',0,100],
+      ['temp',0,100,'°C'],
+      ['pressure',0,2000,'hPa'],
+      ['humidity',0,100,'%'],
+      ['wind',0,100,'m/s'],
   ]
