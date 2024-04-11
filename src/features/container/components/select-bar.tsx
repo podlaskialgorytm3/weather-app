ed6103f1a04b9791c387a273a772ed114c37f089
podlaskialgorytm3 <live.m851@gmail.com>
2024-04-11 18:51:28 +0200

    refactor:  export const to other file

--- src/features/container/components/select-bar.tsx
+++ src/features/container/components/select-bar.tsx
@@ -1,18 +1,8 @@
+import { SELECT_PROPERTIES } from '../constants/select-bar';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { MenuItem } from '@mui/material';
 import { FormControl, InputLabel } from '@mui/material';
 import Box from '@mui/material/Box';
-
-const SELECT_PROPERTIES = [
-    ['temp','from the coldest',true],
-    ['temp','from the hottest',false],
-    ['pressure','from the lowest pressure',true],
-    ['pressure','from the highest pressure',false],
-    ['humidity','from the lowest humidity',true],
-    ['humidity','from the highest humidity',false],
-    ['wind','from the lowest wind',true],
-    ['wind','from the highest wind',false],
-]
 
 export const SelectBar = ({handleSort}: {handleSort: (event: SelectChangeEvent) => void}) => {
     return (
---
+++ src/features/container/constants/select-bar.ts
@@ -0,0 +1,10 @@
+export const SELECT_PROPERTIES: Array<[string,string,boolean]> = [
+    ['temp','from the coldest',true],
+    ['temp','from the hottest',false],
+    ['pressure','from the lowest pressure',true],
+    ['pressure','from the highest pressure',false],
+    ['humidity','from the lowest humidity',true],
+    ['humidity','from the highest humidity',false],
+    ['wind','from the lowest wind',true],
+    ['wind','from the highest wind',false],
+]
