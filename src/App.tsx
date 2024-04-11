9a693c3d7feea77dc325694b06a02922b3acbe64
podlaskialgorytm3 <live.m851@gmail.com>
2024-04-11 19:58:27 +0200

    config: add react router dom to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { Container } from "./features/container/components/container";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./api/query-client";
+import { RouterProvider } from "react-router-dom"
+import { createBrowserRouter } from "react-router-dom"
+import { Error } from "./features/error/components/error";
+
+const routes = createBrowserRouter([
+  { path: "/", element: <Container />,errorElement: <Error />}
+])
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-      <Container/>
+    <RouterProvider router={routes}/>
   </QueryClientProvider>
 )
 
